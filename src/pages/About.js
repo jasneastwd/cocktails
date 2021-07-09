94d19bbdab5fca65a1efc5ebb24080887decf02a
jasneastwd <jse.94@me.com>
2021-07-09 12:01:12 +0100

    moving project into new repo

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About</h1>
+      <p>
+        This is a React web app built to utilise React hooks, styled with CSS.
+      </p>
+      <p>Search for your favourite cocktail and get the recipe!</p>
+    </section>
+  );
+};
+
+export default About;
